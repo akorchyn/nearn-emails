2be01a13d8411f128c268aa8f5280ee757f2c3be
akorchyn <artur.yurii.korchynskyi@gmail.com>
2025-04-18 13:23:51 +0300

    feat: replaced emails with NEARN Team

--- src/constants/emails.ts
+++ src/constants/emails.ts
@@ -1,6 +1,5 @@
 import { PROJECT_NAME } from './project';
 
-export const ceoName = 'Ori';
-export const ceoEmail = `${ceoName} from ${PROJECT_NAME} <${process.env.CEO_EMAIL}>`;
-export const listingsEmail = `${PROJECT_NAME} <${process.env.LISTINGS_EMAIL}>`;
-export const alertsEmail = `${PROJECT_NAME} <${process.env.ALERTS_EMAIL}>`;
+export const ceoEmail = `${PROJECT_NAME} Team <${process.env.CEO_EMAIL}>`;
+export const listingsEmail = `${PROJECT_NAME} Team <${process.env.LISTINGS_EMAIL}>`;
+export const alertsEmail = `${PROJECT_NAME} Team <${process.env.ALERTS_EMAIL}>`;
